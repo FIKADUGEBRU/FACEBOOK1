4dd5625a80d4c990d4d3989a1b3c8be66b5ca690
FIKADUGEBRU <fikadu.gebru3@gmai.com>
2018-10-07 16:07:38 -0400

    FACEBOOK TENT
    SEE TO CHACK

---
+++ FACEBOOK1/github/src/main/java/github/fikadu.java
@@ -0,0 +1,20 @@
+package github;
+
+import org.mule.api.MuleMessage;
+import org.mule.api.routing.MessageInfoMapping;
+
+public class fikadu implements MessageInfoMapping {
+
+	@Override
+	public String getMessageId(MuleMessage message) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCorrelationId(MuleMessage message) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
